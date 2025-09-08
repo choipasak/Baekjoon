354756360b04af2e039cdbea3308ed503831d3a0
Jeonghyun <chlwjdgus822@gmail.com>
2025-09-08 23:28:33 +0900

    ➕ chore: add solution for BOJ
    * 11718
    * 2675 예정

---
+++ src/bronze/No_11718.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.io.*;
+
+public class No_11718 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String line;
+
+    while ((line = br.readLine()) != null) {
+      sb.append(line + "\n");
+    }
+    System.out.print(sb);
+  }
+}
